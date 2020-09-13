c44360c7dc13b66f253158d4807f4e820a1839f5
Andres Cendales <andres01660@gmail.com>
2020-09-12 21:20:29 -0500

    change settings for deploy - initial configuration

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -6,6 +6,9 @@ import os
 #environment variables
 import environ
 
+#Deploy on heroku
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -116,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+django_heroku.settings(locals())
