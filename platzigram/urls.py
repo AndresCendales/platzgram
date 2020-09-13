5030e94a260a586d777a5aeedeb4597e93ebe42e
Andres Cendales <andres01660@gmail.com>
2020-09-13 14:57:03 -0500

    delete modeluser

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,6 +1,7 @@
 """
 platzigram URL Configuration
 """
+#Django
 from django.contrib import admin
 from django.urls import path
 from platzigram import views as local_views
@@ -10,6 +11,7 @@ from posts import views as post_views
 
 
 urlpatterns = [
+    path('admin/',admin.site.urls),
     path('hello-world/', local_views.hello_world),
     path('sorted/', local_views.sort_integers),
     path('say_hi/<str:name>/<int:age>/',local_views.say_hi),
