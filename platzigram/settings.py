91943cef1dbfd2e8f433a670175cb17f137ffc7c
Andres Cendales <andres01660@gmail.com>
2020-09-22 22:30:40 -0500

    Add staticfiles configuration

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -58,7 +58,14 @@ ROOT_URLCONF = 'platzigram.urls'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'templates')
+STATICFILES_DIRS = (
+    os.path.join('static'),
+    )
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 
 TEMPLATES = [
@@ -123,6 +130,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL= '/media/'
 
 django_heroku.settings(locals())
--- platzigram/urls.py
+++ platzigram/urls.py
@@ -5,8 +5,11 @@ platzigram URL Configuration
 from django.contrib import admin
 from django.urls import path
 from platzigram import views as local_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 #Views from posts
+
 from posts import views as post_views
 
 
@@ -16,4 +19,4 @@ urlpatterns = [
     path('sorted/', local_views.sort_integers),
     path('say_hi/<str:name>/<int:age>/',local_views.say_hi),
     path('posts/',post_views.list_posts)
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
