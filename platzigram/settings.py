6836bf37a75aa68d66d56994dcc52880f7a3f1db
Andres Cendales <andres01660@gmail.com>
2020-10-01 21:35:19 -0500

    merge

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -90,12 +90,7 @@ WSGI_APPLICATION = 'platzigram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {'default': env.db('DATABASE_URL')} #Read database in env
 
 
 # Password validation
--- platzigram/urls.py
+++ platzigram/urls.py
@@ -21,8 +21,8 @@ urlpatterns = [
     path('sorted/', local_views.sort_integers, name='sort'),
     path('say_hi/<str:name>/<int:age>/',local_views.say_hi, name='hi'),
     
-    path('/',post_views.list_posts,name='feed'),
-    path('/posts/new/',post_views.create_post,name='create_post'),
+    path('',post_views.list_posts,name='feed'),
+    path('posts/new/',post_views.create_post,name='create_post'),
     
     
     path('users/login/', users_views.login_view,name='login'),
